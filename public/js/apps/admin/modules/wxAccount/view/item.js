10f9012f71dfce5d8976657d6c083b2345621467
大人一号 <gomac@qq.com>
2015-05-11 17:30:59 +0800

    修改className

--- public/js/apps/admin/modules/wxAccount/view/item.js
+++ public/js/apps/admin/modules/wxAccount/view/item.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module) {
 	var View = Backbone.View.extend({
 		tagName: 'div',
-		className: 'col s6',
+		className: 'col s12 m4 l3',
 		template: require('apps/admin/modules/wxAccount/tpl/item.handlebars'),
 		initialize: function(options) { },
 		events: { },
