32f9fad43a4d3a318074eb1c80c23ba98208d54b
大人一号 <gomac@qq.com>
2015-05-13 15:52:16 +0800

    部署微信公众号关键字和粉丝维护

---
+++ public/js/apps/admin/modules/wxAccount/view/keyword.js
@@ -0,0 +1,18 @@
+define(function(require, exports, module) {
+  var dialog = require('MDialog'),
+    AccountModel = require('model/Wechat');
+
+  var View = Backbone.View.extend({
+    el: '#main',
+    template: require('apps/admin/modules/wxAccount/tpl/keywordList.html'),
+    initialize: function(options) {
+      this.$el.html( this.template );
+    },
+    events: {},
+    render: function() {}
+  });
+
+  module.exports = function(options) {
+    return ( new View(options) );
+  }
+});
--- public/js/apps/admin/router.js
+++ public/js/apps/admin/router.js
@@ -4,16 +4,29 @@ define(function(require, exports, module) {
 	};
 	module.exports = Backbone.Router.extend({
 		routes: {
-			'': 'wechatPage'
+			'': 'wxAccount',
+			'account': 'wxAccount',
+			'keyword/:account': 'wxKeyword',
+			'fans/:account': 'wxFans'
 		},
 		currentApp: null,
 		initialize: function() {
 			this.navbar = require('apps/admin/view/navbar')();
 		},
-		wechatPage: function() {
+		wxAccount: function() {
 			if(this.currentApp) this.currentApp.undelegateEvents();
 			this.currentApp = require('apps/admin/modules/wxAccount/view/list')();
 			setTitle('微信公众号列表');
+		},
+		wxKeyword: function() {
+			if(this.currentApp) this.currentApp.undelegateEvents();
+			this.currentApp = require('apps/admin/modules/wxAccount/view/keyword')();
+			setTitle('微信公众号关键字维护');
+		},
+		wxFans: function(account) {
+			if(this.currentApp) this.currentApp.undelegateEvents();
+			this.currentApp = require('apps/admin/modules/wxAccount/view/fans')();
+			setTitle('微信公众号粉丝维护');
 		}
 	});
 });
