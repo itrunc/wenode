81ca180413377aa9a361920cb6d193fa227ae6e3
大人一号 <gomac@qq.com>
2015-05-22 21:51:11 +0800

    添加标题非空检查

--- public/js/model/Blog.js
+++ public/js/model/Blog.js
@@ -8,6 +8,7 @@ define(function(require, exports, module) {
     },
     idAttribute: 'objectId',
     validate: function(attrs, options) {
+      if(_.isEmpty(attrs.title)) return '必须输入文章标题';
       if(_.isEmpty(attrs.markdown)) return '必须输入文章内容';
       if(_.isEmpty(attrs.html)) return '无法获取文章内容';
     },
