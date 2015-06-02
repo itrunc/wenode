4076bf2051f8f4ca3ca930031cee824c3dab1854
大人一号 <gomac@qq.com>
2015-06-02 13:16:04 +0800

    使用IModelWithTags模型进行扩展

--- public/js/model/Blog.js
+++ public/js/model/Blog.js
@@ -1,5 +1,5 @@
 define(function(require, exports, module) {
-  module.exports = Backbone.Model.extend({
+  module.exports = require('model/IModelWithTags').extend({
     defaults: {
       title: '',
       markdown: '',
@@ -7,26 +7,10 @@ define(function(require, exports, module) {
       preview: '',
       tags: []
     },
-    idAttribute: 'objectId',
     validate: function(attrs, options) {
       if(_.isEmpty(attrs.title)) return '必须输入文章标题';
       if(_.isEmpty(attrs.markdown)) return '必须输入文章内容';
       if(_.isEmpty(attrs.html)) return '无法获取文章内容';
-    },
-    toTags: function(tags) {
-      var ts = tags || [];
-      ts = _.isString(ts) ? ts.split(/[,\s]+/) : ts;
-      ts = _.isArray(ts) ? ts : [];
-      ts = _.uniq(_.without(ts, ''));
-      return ts;
-    },
-    addTags: function(tags) {
-      this.set('tags', _.union(this.toTags(tags), this.get('tags')));
-    },
-    removeTag: function(tag) {
-      if(tag.length > 0) {
-        this.set('tags', _.without(this.get('tags'), tag));
-      }
     }
   });
 });
